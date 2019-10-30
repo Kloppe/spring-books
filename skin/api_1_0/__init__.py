c79f382acac00e8268e4c442bfd90f1eb9c48a37
DreamCats <maifeng_cat@outlook.com>
2019-10-30 21:38:47 +0800

    update __init__

---
+++ skin/api_1_0/__init__.py
@@ -0,0 +1,11 @@
+# __author__: Mai feng
+# __file_name__: __init__.py
+# __time__: 2019:10:30:21:29
+
+from flask.blueprints import Blueprint
+
+# 需求url:127.0.0.1:5000/api/1.0/index
+api=Blueprint('api_1_0',__name__,url_prefix='/api/1.0')
+
+# 为了能调用到视图需要导入
+from . import demo
