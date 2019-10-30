c1e2e62c8507e279ae5c212c0464fd24c2b662ba
DreamCats <maifeng_cat@outlook.com>
2019-10-30 21:39:28 +0800

    update manage

--- manage.py
+++ manage.py
@@ -4,3 +4,28 @@
 
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+
+from skin import get_app, db
+
+app=get_app('develop')
+
+#让迁移时，app和数据库建立关联
+Migrate(app,db)
+
+#创建脚本管理器
+manager=Manager(app)
+#增加db脚本命令
+manager.add_command('db',MigrateCommand)
+
+# db init
+# python manage.py db init
+
+# db migrate
+# python manage.py db migrate
+
+# db upgrade
+
+# python manage.py db upgrade
+
+if __name__ == '__main__':
+    manager.run()
