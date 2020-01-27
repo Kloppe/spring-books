08654a013b024d60289f1b655953f65ba1088198
DreamCats <maifeng_cat@outlook.com>
2020-01-27 20:13:54 +0800

    bean的生命周期, 第三种方式

---
+++ spring-annotation/src/main/java/org/example/bean/Dog.java
@@ -0,0 +1,33 @@
+/**
+ * @program SpringBooks
+ * @description: Dog
+ * @author: mf
+ * @create: 2020/01/27 20:11
+ */
+
+package org.example.bean;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Component
+public class Dog {
+
+    public Dog() {
+        System.out.println("dog create...");
+    }
+
+    // 对象创建并赋值之后调用
+    @PostConstruct
+    public void init() {
+        System.out.println("dog init ...");
+    }
+
+    // 对象在销毁之前会调用
+    @PreDestroy
+    public void destroy() {
+        System.out.println("dog destroy...");
+    }
+}
