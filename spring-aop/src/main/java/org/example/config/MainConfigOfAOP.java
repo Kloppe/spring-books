12334b026cd1e32a41e7c181db633cbee5a1f22d
DreamCats <maifeng_cat@outlook.com>
2020-01-30 14:55:23 +0800

    AOP使用小例子

---
+++ spring-aop/src/main/java/org/example/config/MainConfigOfAOP.java
@@ -0,0 +1,14 @@
+/**
+ * @program SpringBooks
+ * @description: AOP配置文件
+ * @author: mf
+ * @create: 2020/01/30 14:54
+ */
+
+package org.example.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MainConfigOfAOP {
+}
