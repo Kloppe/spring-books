a43e3db6d41336012fbf44b295419bea3ac5a762
DreamCats <maifeng_cat@outlook.com>
2020-01-30 15:15:18 +0800

    编写切面类和业务类

---
+++ spring-aop/src/main/java/org/example/aop/MathCalculate.java
@@ -0,0 +1,16 @@
+/**
+ * @program SpringBooks
+ * @description: MathCalculate
+ * @author: mf
+ * @create: 2020/01/30 15:00
+ */
+
+package org.example.aop;
+
+public class MathCalculate {
+
+    public int div(int i, int j) {
+        System.out.println("MathCalculate.div...");
+        return i / j;
+    }
+}
