f38822e621eb3f8bfbe474efbe3add7632919a79
DreamCats <maifeng_cat@outlook.com>
2020-02-02 23:21:44 +0800

    BeanDefinitionRegistryPostProcessor 玩玩

--- spring-annotation/src/main/java/org/example/ext/ExtConfig.java
+++ spring-annotation/src/main/java/org/example/ext/ExtConfig.java
@@ -16,6 +16,24 @@ import org.springframework.context.annotation.Configuration;
  * 1、BeanFactoryPostProcessor：beanFactory的后置处理器；
  *  在BeanFactory标准初始化之后调用，来定制和修改BeanFactory的内容；
  *  所有的bean定义已经保存加载到beanFactory，但是bean的实例还未创建
+ * BeanFactoryPostProcessor原理:
+ *  1)、ioc容器创建对象
+ *  2)、invokeBeanFactoryPostProcessors(beanFactory);
+ *      如何找到所有的BeanFactoryPostProcessor并执行他们的方法；
+ *          1）、直接在BeanFactory中找到所有类型是BeanFactoryPostProcessor的组件，并执行他们的方法
+ *          2）、在初始化创建其他组件前面执行
+*  2、BeanDefinitionRegistryPostProcessor extends BeanFactoryPostProcessor
+ *      postProcessBeanDefinitionRegistry();
+ *      在所有bean定义信息将要被加载，bean实例还未创建的；
+ *      优先于BeanFactoryPostProcessor执行；
+ *      利用BeanDefinitionRegistryPostProcessor给容器中再额外添加一些组件；
+ * 原理：
+ *  1）、ioc创建对象
+ *  2）、refresh()-》invokeBeanFactoryPostProcessors(beanFactory);
+ *  3）、从容器中获取到所有的BeanDefinitionRegistryPostProcessor组件。
+ *      1、依次触发所有的postProcessBeanDefinitionRegistry()方法
+ *      2、再来触发postProcessBeanFactory()方法BeanFactoryPostProcessor；
+*   4）、再来从容器中找到BeanFactoryPostProcessor组件；然后依次触发postProcessBeanFactory()方法
  */
 @ComponentScan("org.example.ext")
 @Configuration
