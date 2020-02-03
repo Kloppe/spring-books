6d676df60a2e9575d4c8845d04fb76ca10c2167d
DreamCats <maifeng_cat@outlook.com>
2020-02-03 12:20:18 +0800

    品 @SpringBootApplication 注解

--- springboot-helloworld/src/main/java/com/example/helloworld/HelloworldApplication.java
+++ springboot-helloworld/src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -3,6 +3,14 @@ package com.example.helloworld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @SpringBootApplication： 这个注解，需要细品，项目启动的入口呀
+ * 主要是三个注解：
+ *  1. @SpringBootConfiguration:允许在上下文中注册额外的bean或导入其他配置类。
+ *  2. @EnableAutoConfiguration:启用 SpringBoot 的自动配置机制
+ *      根据我的猜测，估计是ImportSelect导入组件：AutoConfigurationImportSelector，可以点进去看看
+ *  3. @ComponentScan: 扫描常用的注解
+ */
 @SpringBootApplication
 public class HelloworldApplication {
 
