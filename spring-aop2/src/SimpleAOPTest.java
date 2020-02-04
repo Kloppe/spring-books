3a9ef7a7320893124ef9e6c72ae5a24e22b0ac87
DreamCats <maifeng_cat@outlook.com>
2020-02-04 20:30:40 +0800

    AOP简单实现

--- spring-aop2/src/SimpleAOPTest.java
+++ spring-aop2/src/SimpleAOPTest.java
@@ -9,16 +9,19 @@ public class SimpleAOPTest {
     public static void main(String[] args) {
         // 1. 创建一个 MethodInvocation 实现类 切面逻辑类
         MethodInvocation logTask = () -> System.out.println("log task start");
+        MethodInvocation logTaskEnd = () -> System.out.println("log task end");
         // 业务逻辑类
         HelloServiceImpl helloService = new HelloServiceImpl();
 
         // 2. 创建一个Advice 切入点 切
         BeforeAdvice beforeAdvice = new BeforeAdvice(helloService, logTask);
+        AfterAdvice afterAdvice = new AfterAdvice(helloService, logTaskEnd);
 
         // 3. 为目标对象生成代理
         HelloService helloServiceImplProxy = (HelloService) SimpleAOP.getProxy(helloService, beforeAdvice);
-
+        HelloService helloServiceImplProxyAfter = (HelloService) SimpleAOP.getProxy(helloService, afterAdvice);
         helloServiceImplProxy.sayHelloWorld();
+        helloServiceImplProxyAfter.sayHelloWorld();
 
     }
 }
